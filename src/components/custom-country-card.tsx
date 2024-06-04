71fc651e99cbed4f8295665d4d9002912be15164
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-06-04 19:44:46 +0700

    feat: added routing setting

--- src/components/custom-country-card.tsx
+++ src/components/custom-country-card.tsx
@@ -1,4 +1,5 @@
 import { Country } from "@/utils/api-types"
+import { Link } from "react-router-dom";
 
 interface Props {
     country: Country
@@ -9,7 +10,13 @@ const {country} = props
 
   return (
     <div className="grid grid-rows-2 m-5 justify-center">
-      <img className="h-40 w-80" src={country.flags.png} alt={country.flags.alt} />
+      <Link to={`/`}>
+        <img
+          className="h-40 w-80"
+          src={country.flags.png}
+          alt={country.flags.alt}
+        />
+      </Link>
       <div>
         <p>{country.name.common}</p>
         <p>{country.name.official}</p>
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './pages'
-import { Toaster } from './components/ui/toaster';
-import './styles/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { Toaster } from "@/components/ui/toaster";
+
+import App from "@/routes";
+import "@/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
