e24fb77ebeb44ac5e74564ae21af8eadcf3609e0
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-06-05 04:05:16 +0700

    feat: deleted unused pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Homepage from "@/pages";
-import Details from "@/pages/detail";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Homepage />,
-  },
-  {
-    path: "/country-details/:name",
-    element: <Details />,
   },
 ]);
 
